990f152e490eeb437ec2dcfedeb242e65c089d7e
vamsidhar2003 <128588957+vamsidhar2003@users.noreply.github.com>
2024-03-31 16:05:47 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+# Presettings:
+header = overlayList[0]
+drawColor = (0, 0, 255)
+thickness = 20 # Thickness of the painting
+tipIds = [4, 8, 12, 16, 20] # Fingertips indexes
+xp, yp = [0, 0] # Coordinates that will keep track of the last position of the index finger
